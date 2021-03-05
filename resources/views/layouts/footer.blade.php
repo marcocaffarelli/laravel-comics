9f64fc023d8330a2c00d73c0774c1aa108b2c106
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-03-05 12:23:06 +0100

    Creato un file footer per separarlo dal file app
    Creato un file footer per separarlo dal file app

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,72 @@
+
+<div class="container_footer">
+            <div class="parte_superiore_footer">
+                <ul class="d-flex">
+                    <li><img src="../img/spiderm.jpg" alt="">MARVEL COMICS</li>
+                    <li><img src="../img/iron.jpg" alt="">MARVEL MERCHANDISE</li>
+                    <li><img src="../img/cap.jpg" alt="">PRINT SUBSCRIPTIONS</li>
+                    <li><i class="fas fa-map-marker-alt"></i><span>COMIC SHOP LOCATOR</span></li>
+                </ul>
+            </div>
+            <div class="parte_centrale_footer d-flex">
+                <div class="link_footer d-flex">
+                    <div class="d-flex">
+                        <div>
+                            <ul class="first_ul">
+                                <li><h4>MARVEL COMICS</h4></li>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><h4>SHOP</h4></li>
+                                <li><a href="#">Shop Marvel</a></li>
+                                <li><a href="#">Shop Marvel Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><h4>MARVEL</h4></li>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy Policy (New)</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshop</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Shop Helps</a></li>
+                                <li><a href="#">Contact U</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><h4>SITES</h4></li>
+                                <li><a href="#">MARVEL</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">MARVEL Kids</a></li>
+                                <li><a href="#">MARVEL Universe</a></li>
+                                <li><a href="#">MARVEL Shop</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>All Sites Content TM and &copy; 2020 MARVEL Entertainment, unless otherwise <span>noted here</span>. All rights reserved <span>Cookies Settings</span></div>
+                </div>
+                <div class="parte_vuota"></div>
+            </div>
+            <div class="parte_inferiore_footer">
+                <div class="parte_inferiore_container d-flex">
+                    <div>SIGN-UP NOW!</div>
+                    <div>
+                        <span>FOLLOW US</span>
+                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-youtube"></i>
+                        <i class="fab fa-pinterest"></i>
+                        <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+  
